e9de978d8eb0a58106b1c450c1973fa1499dcbf5
root <root@DESKTOP-FMKOPQQ.localdomain>
2026-03-13 15:56:17 -0300

    <Feat>: The framework for the service project has been created

---
+++ back-end/Program.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "server=localhost;user=root;password=;database=Agendamentos;";
+
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Conexão com MySQL estabelecida com sucesso!");
+
+                string query = "SELECT 1";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        Console.WriteLine("Consulta executada: " + reader.GetInt32(0));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+        }
+    }
+}
