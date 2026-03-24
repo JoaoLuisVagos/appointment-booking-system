bd25497ce4a02f8259c120388a804f7d47f41ddc
João Luis Vagos <joaoluisvagos@gmail.com>
2026-03-24 01:04:09 -0300

    <fix>HorariosController:  The schedule module has been fixed to display the product name and username on the front-end, and improvements have also been made to the error return messages in the API.

--- back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b81ffa60ad03de55d65f847483d5b620c7e444")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc937bae7bd13c5a23ae41a7bb3bb389e067e755")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
